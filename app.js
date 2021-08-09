44fde9ce7e67461ac46b7bd1bd5cbf6938b17f8b
Nikita Dannik <nikitka.dannik@gmail.com>
2021-08-09 18:50:52 +0300

    cors

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const options = {
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'Origin', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Origin'],
   // credentials: true,
 };
 app.use('*', cors(options));
